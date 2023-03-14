7a44ab752430a4550abdf15709c0462a14895033
Vyncen Lara <vyncen.lara@gmail.com>
2023-03-14 09:33:51 -0600

    Agregue el paso 2
    para que haga la prueba para ver si el mayor el par o impar

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ residuo = 0
 
 
 #Paso 2 Vyncen Lara
-
+residuo=mayor%2
+if residuo == 0:
+    print("El número ",mayor," es par porque su residuo es 0")
+else:
+    print("El número ",mayor," es impar porque su residuo es ",residuo,)
 
 
 #Paso 3 Juan Cortés
